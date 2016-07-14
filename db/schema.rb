f59670caf1ccaaa8c8cec5f293e5433572a0a688
Dao Danh Luu <luubkk57@gmail.com>
2016-07-14 13:02:23 +0700

    finish login chuong 9

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160713090011) do
+ActiveRecord::Schema.define(version: 20160714043539) do
 
   create_table "entries", force: :cascade do |t|
     t.string   "title"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20160713090011) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 

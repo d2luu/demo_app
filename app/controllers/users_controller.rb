b5a0b8ddc342b436ae1e945a29317493ee52e17e
Dao Danh Luu <luubkk57@gmail.com>
2016-07-15 14:51:19 +0700

    ABC

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.paginate(page: params[:page], per_page: 10)
   end
 
   # GET /users/1
